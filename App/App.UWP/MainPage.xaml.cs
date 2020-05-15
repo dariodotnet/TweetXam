3f405fa17be45262b4c26e9324d6791ed34370f1
Darío Alonso <dariovillar@hotmail.com>
2020-05-15 18:55:28 +0100

    Iniciando proyecto para todas las plataformas disponibles, Android, iOS y UWP

---
+++ App/App.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace App.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Forms.App());
+        }
+    }
+}
