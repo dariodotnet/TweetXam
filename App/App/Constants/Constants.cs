098c4332d5ec238d144ffe649243d97ac92437f0
Darío Alonso <dariovillar@hotmail.com>
2020-05-25 12:05:01 +0100

    Constantes de conexión a nuestra aplicación de Twitter

---
+++ App/App/Constants/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Forms
+{
+    public class Constants
+    {
+        public static string ConsumerKey => "CONSUMER_KEY";
+        public static string ConsumerSecret => "CONSUMER_SECRET";
+        public static string UserAccessToken => "USER_ACCESS_TOKEN";
+        public static string UserAccessSecret => "USER_ACCESS_SECRET";
+    }
+}
